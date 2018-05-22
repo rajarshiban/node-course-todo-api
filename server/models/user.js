8cf38d43990df68b6e1db0ef8a21159ee9e48ddb
rajarshiban <rajarshi.ban@gmail.com>
2018-05-22 17:38:24 +1000

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -40,7 +40,7 @@ UserSchema.methods.toJSON = function () {
   var userObject = user.toObject();
 
   return _.pick(userObject, ['_id', 'email']);
-}  
+}
 
 UserSchema.methods.generateAuthToken = function () {
   var user = this;
@@ -54,7 +54,24 @@ UserSchema.methods.generateAuthToken = function () {
   })
 }
 
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
 
+  try{
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+      return Promise.reject();
+      };
+
+
+  return User.findOne(
+    {
+      '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  });
+};
 
 var User = mongoose.model('User', UserSchema);
 
