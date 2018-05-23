3727db87f565c2ea0cf50ccd6e4976d7b1571db9
rajarshiban <rajarshi.ban@gmail.com>
2018-05-23 18:53:51 +1000

    add tests GET /users/me and POST /users

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,30 +4,15 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo',
-  completed: true,
-  completedAt: 333
-}
-];
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
-
-beforeEach((done) => {
-  Todo.remove({})
-  .then(() => {
-
-    Todo.insertMany(todos);
-  })
-  .then(() => done());
-});
-
-describe('POST /todos', () => {
-
+//describe('POST /todos', () => {
+describe('POST /todos', function () {
+  this.timeout(5000);
   it('should create a new todo', (done) => {
     var text = 'Test todo text';
 
@@ -245,3 +230,97 @@ describe('GET /todos', () => {
 
 
         })
+
+        describe('GET /users/me', () => {
+
+          it('should return user if authenticated', (done) => {
+
+            request(app)
+                   .get('/users/me')
+                   .set('x-auth', users[0].tokens[0].token)
+                  .expect(200)
+                  .expect((res) => {
+                     expect(res.body._id).toBe(users[0]._id.toHexString());
+                     expect(res.body.email).toBe(users[0].email);
+                  })
+                  .end(done);
+
+                  })
+
+            it('should return 401 if not authenticated', (done) => {
+              request(app)
+                     .get('/users/me')
+                    .expect(401)
+                    .expect((res) => {
+                       expect(res.body).toEqual({});
+                    })
+                    .end(done);
+
+                    })
+
+
+     })
+
+     describe('POST /users', function () {
+
+       it('should create a new user', (done) => {
+         var email = 'mno@example.com';
+         var password = 'APassword';
+
+         request(app)
+                .post('/users')
+               .send({email, password})
+               .expect(200)
+               .expect((res) => {
+                  expect(res.body.email).toBe(email);
+                  expect(res.body._id).toExist();
+                  expect(res.headers['x-auth']).toExist();
+               })
+               .end((err, res) => {
+                 if(err) {
+                   return done(err);
+                 }
+
+                 User.findOne({email}).then((user) => {
+                   expect(user.email).toBe(email);
+                   expect(user.password).toNotBe(password);
+                   done();
+                 })
+               })
+            })
+
+            it('should return validation error if request invalid', (done) => {
+              var email = 'mnoexample.com';
+              var password = 'APas';
+
+              request(app)
+                     .post('/users')
+                    .send({email, password})
+                    .expect(400)
+            /*        .expect((res) => {
+                       expect(res.body.email).toBe(email);
+                       expect(res.body._id).toExist();
+                       expect(res.headers['x-auth']).toExist();
+                    }) */
+                    .end(done);
+
+                 })
+
+                 it('should not create user if email in use', (done) => {
+                   var email = 'john@example.com';
+
+
+                   request(app)
+                          .post('/users')
+                         .send({email})
+                         .expect(400)
+                 /*        .expect((res) => {
+                            expect(res.body.email).toBe(email);
+                            expect(res.body._id).toExist();
+                            expect(res.headers['x-auth']).toExist();
+                         }) */
+                         .end(done);
+
+                      })
+
+       })
